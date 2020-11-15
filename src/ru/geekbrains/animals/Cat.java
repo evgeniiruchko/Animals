0f309a0ac2e1641b944468304adca7884112b2ac
EvgeniiRuchko <gk1987@mail.ru>
2020-11-15 17:07:55 +0300

    пустой

--- src/ru/geekbrains/animals/Cat.java
+++ src/ru/geekbrains/animals/Cat.java
@@ -2,9 +2,9 @@ package ru.geekbrains.animals;
 
 public class Cat extends Animal {
 
-    public Cat (String name, int runLimit, int jumpLimit)
-    {
-        super(name, runLimit, jumpLimit);
-    }
+//    public Cat (String name, int runLimit, int jumpLimit)
+//    {
+//        super(name, runLimit, jumpLimit);
+//    }
 
 }
--- src/ru/geekbrains/animals/Dog.java
+++ src/ru/geekbrains/animals/Dog.java
@@ -1,24 +1,24 @@
 package ru.geekbrains.animals;
 
 public class Dog extends Animal {
-    int swimLimit;
-
-    public Dog(String name, int runLimit, int jumpLimit, int swimLimit)
-    {
-        super(name, runLimit, jumpLimit);
-        this.swimLimit = swimLimit;
-    }
-
-    @Override
-    public void Swim(int length){
-        if (swimLimit == 0){
-            System.out.println(name + " совсем не умеет плавать");
-        }else if (swimLimit > 0 && length == 0){
-            System.out.println(name + " умеет, но не хочет никуда плыть");
-        } else if (length <= swimLimit && length > 0) {
-            System.out.println(name + " плывёт " + length + " метров");
-        } else{
-            System.out.println(name + " не может так плавать");
-        }
-    }
+//    int swimLimit;
+//
+//    public Dog(String name, int runLimit, int jumpLimit, int swimLimit)
+//    {
+//        super(name, runLimit, jumpLimit);
+//        this.swimLimit = swimLimit;
+//    }
+//
+//    @Override
+//    public void Swim(int length){
+//        if (swimLimit == 0){
+//            System.out.println(name + " совсем не умеет плавать");
+//        }else if (swimLimit > 0 && length == 0){
+//            System.out.println(name + " умеет, но не хочет никуда плыть");
+//        } else if (length <= swimLimit && length > 0) {
+//            System.out.println(name + " плывёт " + length + " метров");
+//        } else{
+//            System.out.println(name + " не может так плавать");
+//        }
+//    }
 }
